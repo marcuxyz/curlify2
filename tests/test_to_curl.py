415380caf4d66cdfe47829ea9878142c8b19891c
Marcus Pereira <9499562+marcuxyz@users.noreply.github.com>
2023-06-07 19:15:47 -0300

    📦 chore(pyproject.toml): update requests and responses dependencies (#13)
    🧪 test(test_to_curl.py): update expected curl string
    The requests and responses dependencies have been updated to their latest versions. The expected curl string in the test_to_curl.py file has been updated to reflect the new version of the requests module.

--- tests/test_to_curl.py
+++ tests/test_to_curl.py
@@ -13,7 +13,7 @@ def test_returns_curl_string_using_requests_module():
     curl = to_curl(response.request)
     assert (
         curl
-        == 'curl -X POST -H "User-Agent: python-requests/2.26.0" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -H "Content-Length: 0" -d \'None\' http://1.1.1.1/'
+        == 'curl -X POST -H "User-Agent: python-requests/2.31.0" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -H "Content-Length: 0" -d \'None\' http://1.1.1.1/'
     )
 
 
