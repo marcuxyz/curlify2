3e39bead737fd9cbd6d70f8d6ee37e01210621cd
Marcus Pereira <mpereirassa@gmail.com>
2023-06-08 17:56:26 -0300

    🔧 chore(test_to_curl.py): refactor test_returns_curl_string_using_requests_module and test_returns_curl_string_using_httpx_module The tests have been refactored to use the response object returned by the to_curl function instead of assigning it to a variable and then comparing it to a string. This improves the readability of the tests and makes it easier to identify the source of any failures.

--- tests/test_to_curl.py
+++ tests/test_to_curl.py
@@ -1,19 +1,17 @@
 import httpx
 import requests
-import responses
 from curlify2 import to_curl
 
 URL = "http://1.1.1.1"
 
 
-@responses.activate
 def test_returns_curl_string_using_requests_module():
-    responses.add(responses.POST, URL)
-    response = requests.post(URL)
-    curl = to_curl(response.request)
+    response = requests.get(URL)
+    response = to_curl(response.request)
+
     assert (
-        curl
-        == 'curl -X POST -H "User-Agent: python-requests/2.31.0" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -H "Content-Length: 0" -d \'None\' http://1.1.1.1/'
+        response
+        == 'curl -X GET -H "User-Agent: python-requests/2.31.0" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -d \'None\' https://1.1.1.1/'
     )
 
 
@@ -22,9 +20,10 @@ def test_returns_curl_string_using_httpx_module(httpx_mock):
 
     with httpx.Client() as client:
         response = client.get("http://test_url")
-        curl = to_curl(response.request)
+        response = to_curl(response.request)
+
         assert (
-            curl
+            response
             == 'curl -X GET -H "host: test_url" -H "accept: */*" -H "accept-encoding: gzip, deflate" -H "connection: keep-alive" -H "user-agent: python-httpx/0.24.1" -d '
             "'"
             "b"
